a89b4452eb0a0030d48437f8381149da467f3380
上村一平 <ippeikamimura@mbp.local>
2018-04-08 20:00:32 +0900

    first commit

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,43 @@
+class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
+  def index
+    @boards = Board.page(params[:page]).order(created_at: 'desc')
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    board = Board.create(board_params)
+    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+    redirect_to board_path(board.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+    redirect_to board_path(params[:id])
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
+end
