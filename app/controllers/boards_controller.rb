2c693c400247eb418596107e6484df994f28b9a2
上村一平 <ippeikamimura@mbp.local>
2018-04-09 15:34:06 +0900

    様々な学習。復習の必要あり。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,28 +2,43 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.page(params[:page]).order(created_at: 'desc')
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
-    board = Board.create(board_params)
-    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
-    redirect_to board_path(board.id)
+    board = Board.new(board_params)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+      redirect_to board_path(board.id)
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+      }
+    end
   end
 
   def show
+    @comment = Comment.new(board_id: @board.id)
   end
 
   def edit
   end
 
   def update
-    @board.update(board_params)
-    redirect_to board_path(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(params[:id])
+    else
+      redirect_to :back, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
   end
 
   def destroy
@@ -33,7 +48,7 @@ class BoardsController < ApplicationController
 
   private
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
 
   def set_target_board
